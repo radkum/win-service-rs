6f00f6f58da75e7e6fa2af81eb76b131e8bc09ab
radkum <radoslaw.kumorekit@gmail.com>
2024-09-20 12:36:04 +0200

    support unprotecting service

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,22 @@
 mod action;
 
-use std::ffi::OsString;
+use std::{
+    ffi::{c_void, CString, OsString},
+    ptr::null_mut,
+};
 
 use windows_service::{
     define_windows_service,
     service::{
         ServiceControl, ServiceControlAccept, ServiceExitCode, ServiceState, ServiceStatus,
-        ServiceType,
+        ServiceType, UserEventCode,
     },
     service_control_handler::{self, ServiceControlHandlerResult},
     service_dispatcher,
+};
+use windows_sys::Win32::{
+    Foundation::{GetLastError, FALSE},
+    System::Services::*,
 };
 
 use crate::action::Action;
@@ -32,8 +39,18 @@ fn my_service_main(arguments: Vec<OsString>) {
     }
 }
 
-fn run_service(_arguments: Vec<OsString>) -> windows_service::Result<()> {
+const UNPROTECT: UserEventCode = unsafe { UserEventCode::from_unchecked(0x00000080) };
+
+fn run_service(arguments: Vec<OsString>) -> windows_service::Result<()> {
     let (sender, receiver) = std::sync::mpsc::channel::<Action>();
+
+    let mut service_name = String::new();
+
+    if arguments.len() > 0 {
+        if let Ok(str) = arguments[0].clone().into_string() {
+            service_name = str
+        }
+    }
 
     let event_handler = move |control_event| -> ServiceControlHandlerResult {
         match control_event {
@@ -42,6 +59,11 @@ fn run_service(_arguments: Vec<OsString>) -> windows_service::Result<()> {
                 ServiceControlHandlerResult::NoError
             },
             ServiceControl::Interrogate => ServiceControlHandlerResult::NoError,
+            ServiceControl::UserEvent(user_event_code) => match user_event_code {
+                UNPROTECT => unprotect_current_service(service_name.as_str()),
+                _ => ServiceControlHandlerResult::NotImplemented,
+            },
+
             _ => ServiceControlHandlerResult::NotImplemented,
         }
     };
@@ -97,4 +119,44 @@ fn service_status(
         wait_hint: std::time::Duration::default(),
         process_id: None,
     }
+}
+
+fn unprotect_current_service(service_name: &str) -> ServiceControlHandlerResult {
+    fn get_last_error() -> u32 {
+        unsafe { GetLastError() }
+    }
+
+    // todo: give only necessary rights
+    let h_manager = unsafe { OpenSCManagerA(null_mut(), null_mut(), SC_MANAGER_ALL_ACCESS) };
+    if h_manager.is_null() {
+        log::error!("Failed to call OpenSCManagerA: {}", get_last_error());
+        return ServiceControlHandlerResult::Other(1);
+    }
+
+    let service_name = CString::new(service_name).unwrap();
+
+    // todo: give only necessary rights. Maybe: SERVICE_USER_DEFINED_CONTROL
+    let h_service =
+        unsafe { OpenServiceA(h_manager, service_name.as_ptr() as *const u8, SERVICE_ALL_ACCESS) };
+    if h_service.is_null() {
+        log::error!("Failed to call OpenServiceA: {}", get_last_error());
+        return ServiceControlHandlerResult::Other(1);
+    }
+
+    let info: SERVICE_LAUNCH_PROTECTED_INFO =
+        SERVICE_LAUNCH_PROTECTED_INFO { dwLaunchProtected: SERVICE_LAUNCH_PROTECTED_NONE };
+    let status = unsafe {
+        ChangeServiceConfig2A(
+            h_service,
+            SERVICE_CONFIG_LAUNCH_PROTECTED,
+            &info as *const SERVICE_LAUNCH_PROTECTED_INFO as *const c_void,
+        )
+    };
+
+    if status == FALSE {
+        log::error!("Failed to call ChangeServiceConfig2A: {}", get_last_error());
+        return ServiceControlHandlerResult::Other(1);
+    }
+
+    ServiceControlHandlerResult::NoError
 }
